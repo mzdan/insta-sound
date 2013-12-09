c7eb55f59d0ed2785979fd1a509e99a7df8f620f
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-12-09 02:12:56 -0500

    Adds basic vertical lines for each instagram post, scaling the line positions according to the date they were published.

--- data.js
+++ data.js
@@ -1,3 +1,39 @@
-d3.tsv('nyc_sample.tsv', function(d) {
-    instagram_data = d;
+d3.tsv('nyc_sample.tsv', function(error, data) {
+    instagram_data = data;
+
+    var width = 500;
+    var height = 40;
+
+    data.forEach(function(d){
+        d.publishedDate = new Date(d.published);
+    });
+
+
+    var firstPublishedDate = instagram_data[0].publishedDate;
+    var lastPublishedDate = instagram_data[instagram_data.length - 1].publishedDate;
+
+    var timeScale = d3.scale.linear()
+        .domain([firstPublishedDate, lastPublishedDate])
+        .range([0, width]);
+
+
+    d3.select('#timeseries').append('svg')
+        .attr('width', width + 'px')
+        .attr('height', '40px')
+        .selectAll('.vline')
+        .data(data)
+        .enter()
+        .append('line')
+        .attr("x1", function(d) {
+            return timeScale(d.publishedDate);
+        })
+        .attr("x2", function(d) {
+            return timeScale(d.publishedDate);
+        })
+        .attr("y1", function(d) {
+            return 0;
+        })
+        .attr("y2", height)
+        .style("stroke", "#000");
+
 });
