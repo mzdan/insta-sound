1a94daf280e11f78f01d36129ca5396c3a998772
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2014-01-04 20:36:19 -0500

    Split out web plots. Augment with additional neighborhood plots.

--- Rpackage/R/neighborhood-stats.R
+++ Rpackage/R/neighborhood-stats.R
@@ -7,14 +7,28 @@
 calculate_neighborhood_stats <- function(posts) {
 
     neighborhood_stats <- ddply(
-        posts,
+        posts[posts$neighborhood != '',],
         .(neighborhood),
         summarize,
         users=length(unique(name)),
         average_posts_per_user=length(name)/length(unique(name)),
         posts=length(published)
     )
+    # Reorder the neighborhood factor by top posts
+    neighborhood_stats$neighborhood <- factor(neighborhood_stats$neighborhood,
+                                              levels=rev(neighborhood_stats$neighborhood))
 
+    neighborhood_stats
+
+}
+
+#' Melts neighborhood stats into a form that makes it easier to create some types of plots.
+#' @param neighborhood_stats generated from posts
+melt_neighborhood_stats <- function(neighborhood_stats) {
+    neighborhood_stats_narrow <- melt(neighborhood_stats, c('neighborhood'))
+    neighborhood_stats_narrow$variable <- factor(neighborhood_stats_narrow$variable,
+                                                 levels=c('posts', 'users', 'average_posts_per_user'))
+    neighborhood_stats_narrow
 }
 
 #' Plots neighborhood stats.
@@ -25,6 +39,32 @@ calculate_neighborhood_stats <- function(posts) {
 #' neighborhood_stats <- calculate_neighborhood_stats(posts_sample)
 #' plot_neighborhood_stats(neighborhood_stats)
 plot_neighborhood_stats <- function(stats) {
-    ggplot(stats, aes(x=users, y=neighborhood)) + geom_bar()
+    ggplot(
+        stats,
+        aes(
+            x=posts,
+            y=neighborhood,
+            size=average_posts_per_user,
+            color=users
+         )
+    ) + geom_point()
 }
 
+#' Plots neighborhood stats.
+#' @param stats neighborhood stats
+#' @export
+#' @examples
+#' data(posts_sample)
+#' neighborhood_stats <- calculate_neighborhood_stats(posts_sample)
+#' plot_neighborhood_stats(neighborhood_stats)
+plot_neighborhood_stats_log_scale <- function(stats) {
+    ggplot(
+        stats,
+        aes(
+            x=log(posts),
+            y=neighborhood,
+            size=average_posts_per_user,
+            color=users
+         )
+    ) + geom_point()
+}
--- Rpackage/R/plot_all_web_images.R
+++ Rpackage/R/plot_all_web_images.R
@@ -6,26 +6,49 @@
 #' data(posts_sample)
 #' plot_all_web_images(posts_sample, tempdir())
 plot_all_web_images <- function(posts, path) {
+    subpath <- function(name) file.path(path, name)
+    plot_nyc_web_images(posts, subpath('nyc'))
+    plot_neighborhood_web_images(posts, subpath('neighborhoods'))
+}
 
-    overall_path <- file.path(path, 'overall')
-    dir.create(overall_path)
-    plot_field_densities(posts, overall_path)
+#' Generates nyc plots.
+#' @param posts instagram posts for all neighborhoods
+#' @param path path in which to store the plots
+#' @export
+#' @examples
+#' data(posts_sample)
+#' plot_nyc_web_images(posts_sample, tempdir())
+plot_nyc_web_images <- function(posts, path) {
+    dir.create(path)
+    plot_field_densities(posts, path)
 
-    save_overall_plot <- function(filename, plot_to_save, height=4, width=4) {
-        filepath <- file.path(overall_path, sprintf("%s.png", filename))
+    save_plot <- function(filename, plot_to_save, height=4, width=4) {
+        filepath <- file.path(path, sprintf("%s.png", filename))
         ggsave(filepath, p, height=height, width=width)
     }
 
-    p <- plot_tod_polar(posts)
-    save_overall_plot('nyc_tod_polar', p)
+    save_plot('nyc_tod_polar', plot_tod_polar(posts))
 
-    # TODO: Include only posts which have a neighborhood
+    neighborhood_stats <- calculate_neighborhood_stats(posts)
+    save_plot('neighborhood_stats', plot_neighborhood_stats(neighborhood_stats))
+    save_plot('neighborhood_stats_log_scale', plot_neighborhood_stats_log_scale(neighborhood_stats))
+}
+
+#' Generates individual neighborhood stats plots.
+#' @param posts instagram posts for all neighborhoods
+#' @param path path in which to store the plots
+#' @export
+#' @examples
+#' data(posts_sample)
+#' plot_neighborhood_web_images(posts_sample, tempdir())
+plot_neighborhood_web_images <- function(posts, path) {
+
     d_ply(
         posts,
         .(neighborhood),
         function(d) {
 
-            neighborhood_path <- file.path(path, 'neighborhoods', d[1,'neighborhood'])
+            neighborhood_path <- file.path(path, d[1,'neighborhood'])
             dir.create(neighborhood_path, recursive=TRUE)
             print(sprintf("Plotting: %s", neighborhood_path))
 
