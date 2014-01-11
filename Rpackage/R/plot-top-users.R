c05525688ef9eb1760ca1f083d5f925ac7fd139c
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2014-01-10 23:35:14 -0500

    Remove old top_user_tod png.

--- Rpackage/R/plot-top-users.R
+++ Rpackage/R/plot-top-users.R
@@ -3,21 +3,33 @@
 #' @export
 #' @examples
 #' data(posts_sample)
-#' plot_top_users_map(posts_sample)
-plot_top_users_map <- function(posts) {
-    top_users <- calculate_top_users(posts, 9)
-    top_user_posts <- posts[posts$name %in% top_users$name,]
+#' top_user_posts <- get_top_user_posts(posts_sample)
+#' plot_top_users_map(top_user_posts)
+plot_top_users_map <- function(top_user_posts) {
 
-    plot_base_map(posts) +
-    top_users <- calculate_top_users(posts, 9)
-    top_user_posts <- posts[posts$name %in% top_users$name,]
-
-    plot_base_map(posts) +
+    plot_base_map(top_user_posts) +
         geom_point(data=top_user_posts, aes(x=longitude, y=latitude, color=published_tod/60)) +
         scale_color_tod() +
         facet_wrap(~name) +
         empty_theme_with_legend()
 
+}
+
+#' Plots the polar histogram of TOD with user names as facets
+#' @export
+plot_top_users_tod_polar(top_user_posts) {
+    plot_tod_polar(top_user_posts) + facet_wrap(~name)
+}
+
+#' Reduces posts to only thos of top users.
+#' @export
+get_top_user_posts <- function(posts, limit=9) {
+    top_users <- calculate_top_users(posts, limit)
+    top_user_posts <- posts[posts$name %in% top_users$name,]
+
+    plot_base_map(posts) +
+    top_users <- calculate_top_users(posts, limit)
+    posts[posts$name %in% top_users$name,]
 }
 
 #' Gets an ordered data frame of the top users
